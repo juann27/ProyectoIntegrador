b602442a6feca96733e26872499a1c27d07b6d4e
Juan Pablo Alegría <juan@MacBook-Air-de-Juan.local>
2019-07-16 19:42:50 -0500

    test

--- Busquedas.php
+++ Busquedas.php
@@ -3,32 +3,63 @@
         <title>Busquedas</title>
     </head>
     <body>
-        <?php
-            require 'Conexion.php';
-        ?>
         <form method="POST">
             <input type="text" name="busqueda" value="" />
-            <input type="submit" value="Enviar" name="enviar" />
+            <input type="submit" name="servicio" value="Servicio"/>
+             <input type="submit" name="taller" value="Taller"/>
         </form>
-        
         <?php
+            require 'Conexion.php';
+            
             $busqueda = $_POST['busqueda'];
-            if ($busqueda == "talleres") {
-                $sql = "SELECT * FROM Talleres";
-                $stmt = sqlsrv_query($conn, $sql);
-                
-                while ($_row=sqlsrv_fetch_array($_resultado)){
-                    echo $row[0];
-                }
+            
+            if (empty($busqueda)) {
+                echo "";
             } else {
-                $sql = "SELECT * FROM Servicios";
-                $stmt = sqlsrv_query($conn, $sql);
-                
-                while ($_row=sqlsrv_fetch_array($_resultado)){
-                    echo $row[0];
+                if (isset($_POST['servicio'])) {
+                    $sql = "SELECT * FROM Servicio WHERE descripcion like '%".$busqueda."%'";
+                    $stmt = sqlsrv_query($_conn, $sql);
+
+                    while ($_row=sqlsrv_fetch_array($stmt)){
+                        echo $_row[2] . " ";
+                        echo "<br>";
+                    }
+                } else {
+                    $sql = "SELECT * FROM Taller WHERE nombre like '%".$busqueda."%'";
+                    $stmt = sqlsrv_query($_conn, $sql);
+
+                    while ($_row=sqlsrv_fetch_array($stmt)){
+                        echo $_row[1] . " ";
+                        echo "<br>";
+                    }
                 }
             }
+            
+            
+                
+            
+            /*if ($busqueda == "talleres") {
+                $sql = "SELECT * FROM Taller";
+                $stmt = sqlsrv_query($_conn, $sql);
+                
+                while ($_row=sqlsrv_fetch_array($stmt)){
+                    echo $_row[1] . " ";
+                    echo $_row[2] . " ";
+                    echo $_row[3] . " ";
+                    echo $_row[4] . " ";
+                    echo "<br>";
+                }
+            } else {
+                $sql = "SELECT * FROM Servicio";
+                $stmt = sqlsrv_query($_conn, $sql);
+                
+                while ($_row=sqlsrv_fetch_array($stmt)){
+                    echo $_row[2] . " ";
+                    echo "<br>";
+                }
+            }*/
         ?>
+        
     </body>
 </html>
 
